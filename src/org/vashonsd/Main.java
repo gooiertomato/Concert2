15c62dbbc00c39043fef787684ea5b31ff9e0dca
odegaard.jackson <jackodegaard05@gmail.com>
2021-11-01 11:04:18 -0700

    Added new code

--- src/org/vashonsd/Main.java
+++ src/org/vashonsd/Main.java
@@ -6,13 +6,11 @@ public class Main {
         Performer dingle = new Performer("dingle","tall, looks like Alwyn");
         dingle.addHit("dingle theme song");
         System.out.println(dingle);
-    /**
-     * For now, use the main() method to print out the name of a show.
-     * Create several Performers by giving them names and descriptions.
-     * If you want a performer to have had one or more hit songs, use the performer's
-     * .addHit() method along with the name of the hit.
-     * Then print out the performers.
-     */
+
+        Performer vault = new Performer("vault","short, blue spikey hair");
+        vault.addHit("flight of the whales");
+        vault.addHit("west hemisphere orchastra No.26");
+        System.out.println(vault);
 
 
     }
